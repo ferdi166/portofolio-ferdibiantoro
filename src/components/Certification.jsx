c03dfcbca8f43fa85f6807c5ef9be9d42e1c3747
ferdi166 <ferdiprayoga@akb.ac.id>
2025-09-18 16:28:11 +0700

    Perbaikan section project

--- src/components/Certification.jsx
+++ src/components/Certification.jsx
@@ -156,14 +156,6 @@ const Certification = () => {
                         </div>
                     ))}
                 </div>
-
-                {/* Call to Action */}
-                <div className="text-center mt-16">
-                    <div className="inline-flex items-center space-x-2 bg-gradient-to-r from-blue-600 to-purple-600 text-white px-6 py-3 rounded-full font-semibold hover:from-blue-700 hover:to-purple-700 transition-all duration-300 hover:scale-105">
-                        <FaAward className="w-5 h-5" />
-                        <span>Lihat Semua Sertifikat</span>
-                    </div>
-                </div>
             </div>
         </section>
     )
